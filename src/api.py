d9db32a2fa5b6c65ee8d984c2c32c3d108b47640
drewjel <andrew.james.elsey@gmail.com>
2021-07-21 14:04:59 -0400

    Add support for highlighting hidden terms

--- src/api.py
+++ src/api.py
@@ -49,7 +49,7 @@ def read_data():
         for ln_ in p.stdout:
             lns_.append(ln_.decode('utf-8'))
         if(len(lns_)==0):
-            time.sleep(.125)
+            time.sleep(.25)
     return lns_
 
 def send_cmd(cmd, wait = True):
@@ -118,6 +118,7 @@ def get_state(fname = None):
         coords2 = []
         interps2 = []
         types2 = []
+        error_msgs = []
         for resp_ in resp:
             #print(resp_)
             if 'Existing Interpretation' in resp_:
@@ -138,8 +139,13 @@ def get_state(fname = None):
                 types2.append(m.group(1).strip())
             else:
                 pass
+            
+            if "Error Message: " in resp_:
+                print('found error')
+                error_msgs.append(resp_.strip().split("Error Message: ")[1])
+                print(error_msgs[-1])
                 #print('not type')
-
+        '''
         send_cmd("4\n",False)
         error_data = read_data()
         print('ERROR DATA:')
@@ -153,7 +159,46 @@ def get_state(fname = None):
                 print(error_msgs[-1])
             else:
                 print('not found!')
+        '''
+        send_cmd('4\n', False)
+        resp = read_data()
+        print('ALL DATA DUMP:')
+        list_print(resp)
+        print('get coords response:::')
+        #print()
+        all_coords = []
+        all_interps = []
+        all_types = []
+        all_error_msgs = []
+        for resp_ in resp:
+            #print(resp_)
+            if 'Existing Interpretation' in resp_:
+            #    print(' existing')
+                all_interps.append(resp_.strip())
+            else :
+                pass 
+            #    print('not existing')
+            if 'Begin' in resp_ and 'End' in resp_:
+            #    print(' beginning')
+                all_coords.append(resp_.strip())
+            else:
+                pass
+            #    print('not beginning')
+            m = re.search('Node Type : ([\w]*),',resp_)
+            if m :
+                #print(' type')
+                all_types.append(m.group(1).strip())
+            else:
+                pass
             
+            if "Error Message: " in resp_:
+                print('found error')
+                all_error_msgs.append(resp_.strip().split("Error Message: ")[1])
+                print(all_error_msgs[-1])
+                #print('not type')
+
+        print('exited this part')
+
         coord_array = coords2#coordinates.splitlines()
         interp_array = interps2#interps.splitlines()
         type_array = types2#types.splitlines()
@@ -183,6 +228,29 @@ def get_state(fname = None):
                     "error": error_msgs[i]
                 }
         
+        print('proceeding to alldata')
+        all_data = [None] * len(all_coords)
+        for i in range(len(all_coords)):
+            begin = all_coords[i].split("Begin: ")[1].split("\t")[0]
+            begin_line = int(begin.split("line ")[1].split(",")[0])-1
+            begin_col = int(begin.split("column ")[1])-1
+            end = all_coords[i].split("End:")[1]
+            end_line = int(end.split("line ")[1].split(",")[0])-1
+            end_col = int(end.split("column ")[1])
+            all_data[i] = \
+                {
+                    "coords": {
+                        "begin": {"line": begin_line, "character": begin_col}, 
+                        "end": {"line": end_line, "character": end_col}}, 
+                    "interp": all_interps[i].split("Existing Interpretation: ")[1], 
+                    "node_type": all_types[i],
+                    "error": all_error_msgs[i]
+                }
+        print('ALL TERMS:')
+        for at_ in all_data:
+            print(at_)
+        print('printed all terms')
+
         send_cmd("5\n0\n1\n", False)
         cons_str = read_data()
         print(cons_str)
@@ -217,7 +285,10 @@ def get_state(fname = None):
                 "name": peirce_cnames[i]        }
         #print('PRINTING C DATA')
         #print(cdata)
-        return data, cdata
+
+        print('RETURNING')
+
+        return data, cdata, all_data
     except Exception as e:
         print(e)
 
@@ -240,17 +311,21 @@ def populate():
     global temp_file_name
     temp_file_name = file_name if file_name else _temp_file_name 
 
-    data, cdata = get_state()
+    data, cdata, all_data = get_state()
+    while(len(data) > 50):
+        data, cdata, all_data = get_state()
+
     notes = content["terms"]
     response = app.response_class(
-        response=json.dumps({'data':data,'cdata':cdata}),
+        response=json.dumps({'data':data,'cdata':cdata,'adata':all_data}),
         status=200,
         mimetype='application/json'
     )
-    if(len(data) > 20):
-        print('whats going on here???')
-        return
-    print(json.dumps({'data':data,'cdata':cdata}))
+    
+    print('alldataresult:')
+    for ad_ in all_data:
+        print(ad_)
+    print(json.dumps({'data':data,'cdata':cdata, 'adata':all_data}))
     return response
 
 
@@ -313,7 +388,7 @@ def createTermInterpretation():
     content = request.get_json()
     term = content["term"]
     idx = 7
-    data, cdata = get_state()
+    data, cdata, adata = get_state()
     for i in range(len(data)):
         term_ = data[i]
         if \
@@ -330,7 +405,9 @@ def createTermInterpretation():
     print(term)
 
     send_cmd(str(idx)+"\n",False)
-    print(read_data())
+    list_print(read_data())
+
+
     interp = term["interpretation"]
     name = interp["name"]
     interp_type = interp["interp_type"]
@@ -435,7 +512,7 @@ def createConstructorInterpretation():
     content = request.get_json()
     constructor = content["constructor"]
     idx = 2
-    data, cdata = get_state()
+    data, cdata, adata = get_state()
     print('CONSTRUCTOR : ')
     print(constructor)
     send_cmd("5\n", False)
@@ -513,7 +590,7 @@ def createConstructorInterpretation():
 def check2():
     content = request.get_json()
     terms = content["terms"]
-    data, cdata = get_state()
+    data, cdata, adata = get_state()
     print('terms!!!')
     print(terms)
     print('end terms!!')
@@ -531,6 +608,22 @@ def check2():
     print(terms)
     response = app.response_class(
         response=json.dumps(terms),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+@app.route('/api/check3', methods=["POST"])
+def check3():
+    content = request.get_json()
+    
+    data, cdata, adata = get_state()
+
+    print('RETURNING ADATA')
+    list_print(adata)
+
+    response = app.response_class(
+        response=json.dumps(adata),
         status=200,
         mimetype='application/json'
     )
